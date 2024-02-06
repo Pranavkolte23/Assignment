c1e80896837e1fd95fcc7db4c1a0a817fdbd3623
Pranavkolte23 <129721570+Pranavkolte23@users.noreply.github.com>
2024-02-07 01:10:46 +0530

    Final

--- index.js
+++ index.js
@@ -1,53 +1,95 @@
-//import express from "express"
+const express = require("express");
 
-const express = require('express');
-
-//create instace of express
 const app = express();
+
+//to parse the request body
 app.use(express.json());
 
 
-//create a route
-
-app.get("/ping",(req,res) =>
-{
-    res.send("Pong");
-});
-
-app.get("/Pranav",(req,res) =>
-{
-    res.send("user is pranav");
-});
-
-
-app.post("/login" ,async(req,res)=>
-{
-    try{
-        const userName = req.body.username;
-    const password = req.body.password;
-
-    if(!password)
+//Books Api Routes: GET, POST, PUT, DELETE
+let books = [
     {
-        res.send("invalid")
-    }
-    else if(password === "1234")
+        id: 1,
+        name: "Harry Potter",
+        author: "J.K. Rowling"
+    },
     {
-        res.send("login successful");
-    }
-    else{
-        res.send("invalid");
-    }
-    }
-    catch(e)
+        id: 2,
+        name: "The Alchemist",
+        author:"anymous"
+    },
     {
-        console.log(e)
+        id: 3,
+        name: "The Da Vinci Code",
     }
+
+];
+
+
+app.get("/", (req, res) => {
+    
+    res.status(200).json({ "Books_information" : books });
+})
+app.get("/ping", (req, res) => {
+    res.status(200).json({ message: "Pong" });
+})
+
+
+// Add other requests GET, POST, PUT, DELETE
+
+app.post("/addBook",(req,res)=>{
+   
+    let book = {
+        id: books[books.length - 1].id + 1,
+        name: req.body.name,
+        author : req.body.author
+      };
+      books.push(book);
+      console.log(books)
+
+    res.status(200).json({"message" : "Book Added Successfully"});
+    
     
 
 });
-//start server
 
-app.listen(8000,()=>
-{
-    console.log("server is live on :8000 ");
+
+
+app.put("/updateBook/:id",(req,res)=>{
+    const objectId = parseInt(req.params.id);
+    const updatedObject = req.body; 
+
+   
+    const index = books.findIndex(obj => obj.id === objectId);
+    
+    if (index !== -1) {
+        
+        books[index] = { ...books[index], ...updatedObject };
+        res.json({ message: 'Object updated successfully', updatedObject: books[index] });
+    } else {
+        res.status(404).json({ message: 'Object not found' });
+    }
+
+
+});
+    
+
+app.delete('/deleteBook/:id', (req, res) => {
+    const recordId = parseInt(req.params.id);
+
+    // Find the index of the record with the given ID in the array
+    const index = books.findIndex(record => record.id === recordId);
+
+    // If record with given ID is found
+    if (index !== -1) {
+        // Remove the record from the array
+        books.splice(index, 1);
+        res.json({ message: 'Record deleted successfully' });
+    } else {
+        res.status(404).json({ message: 'Record not found' });
+    }
+});
+
+app.listen(8000, () => {
+    console.log(`App is live on: http://localhost:8000`);
 });
